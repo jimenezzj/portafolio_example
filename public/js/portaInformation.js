f598e6759e463e0eb2f851de134f6a6f0f60d6d7
Jonathan <jjimenezz@ucenfotec.ac.cr>
2020-07-06 20:30:42 -0600

    Initial commit

---
+++ public/js/portaInformation.js
@@ -0,0 +1,94 @@
+const info = {
+    EN: {
+        intro: {
+            greenting: 'Her there, I am',
+            pos: 'WEB DEV',
+            name: 'Jonathan'
+        },
+        services: {
+            title: 'What I do?',
+            subTitles: {
+                front: {
+                    title: 'Front-end Development',
+                    subtitles: ['Languages I speak', 'Tools, Frameworks & Others']
+                },
+                back: {
+                    title: 'Back-end Development',
+                    subtitles: ['Languages I speak', 'Tools, Frameworks & Others']
+                },
+                ui: {
+                    title: 'UI/UX Design',
+                    subtitles: ['Designer Tools']
+                }
+            }
+        },
+        about: {
+            title: 'Who I am?',
+            subTitle: 'WEB DEV BASED IN SAN FRANCISCO, HEREDIA',
+            description: [
+                "My name is Jonathan Jiménez Zambrana, I'm a passionate software developer who aims to build well structured and maintainable appplications.I consider myself a self - taugh as well as a quick learning who tries to be up - to - date with most recent technologies updates.I like to help people, cooperate with them and learn from them.I've worked in several educational and profesional projects on different areas such as: web development(front-end & back-end), dektop apps and games development"
+                , "I’m currently studying at Centotec university to achieve a software development degree. I already got software development technician meanwhile I was in high school. Also I’ve worked for two companies in both as web developer using different stack of web technologies to build professional, maintainable and beautiful designs web apps."
+            ]
+        },
+        work: {
+            title: 'My Work',
+            subTitle: 'A section with some of my works'
+        },
+        buttons: {
+            work: 'my work',
+            viewMore: 'view more'
+        },
+        navbar: {
+            intro: 'Home',
+            services: 'My Services',
+            aboutme: 'About me',
+            work: 'My Work',
+        }
+    },
+    ES: {
+        intro: {
+            greenting: 'Hola, Soy ',
+            pos: 'WEB DEV',
+            name: 'Jonathan!'
+        },
+        services: {
+            title: '¿Qué es lo que hago?',
+            subTitles: {
+                front: {
+                    title: 'Desarrollador Front-end',
+                    subtitles: ['Lenguajes que hablo', 'Herramientas, Frameworks & Otros']
+                },
+                back: {
+                    title: 'Desarrollador Back-end',
+                    subtitles: ['Lenguajes que hablo', 'Herramientas, Frameworks & Otros']
+                },
+                ui: {
+                    title: 'Diseño UI/UX',
+                    subtitles: ['Herramientos de diseño']
+                }
+            }
+        },
+        about: {
+            title: '¿Quien soy?',
+            subTitle: 'WEB DEV CON BASE EN SAN FRANCISCO, HEREDIA',
+            description: [
+                "Mi nombre es Jonathan Jiménez Zambrana, Soy un desarrollador de software quien disfruta de la construcción de aplicaciones bien estructuradas y mantenibles. Me considero a mí mismo como autodidacta, así como alguien que aprende rápido para intentar estar tan actualizado con las nuevas tecnologías como sea posible. Me gusta ayudar a las personas, cooperar con las misas y aprender de ellas. He trabajado en varios proyectos tanto educativos como profesionales en diferentes áreas como: desarrollo web (front-end & back-end), desarrollo de aplicaciones de escritorio y desarrollos de video juegos."
+                , "Actualmente me encuentro estudiando en la universidad de Cenfotec la carrera de desarrollo de software. Sin embargo, cuento con un técnico en desarrollo de software mientras estaba en el colegio. También en trabajando para dos compañías en ambas en el ámbito de desarrollo de software en las utilice diferentes conjuntos de tecnologías web para construir aplicaciones web profesionales, mantenibles y bonitas."
+            ]
+        },
+        work: {
+            title: 'Mi Trabajo',
+            subTitle: 'Una sección con algunos de mis trabajos'
+        },
+        buttons: {
+            work: 'mi trabajo',
+            viewMore: 'ver mas'
+        },
+        navbar: {
+            intro: 'Principal',
+            services: 'Mi Servicios',
+            aboutme: 'Acerca de mi',
+            work: 'My Trabjo',
+        }
+    }
+}
